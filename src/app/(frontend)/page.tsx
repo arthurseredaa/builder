02e22ffa8af6dd83decd1d937c62f503aa34676d
arthursereda <neonbetcy@gmail.com>
2025-02-16 01:17:22 +0200

    chore: remove unnecessary features from home page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,9 +1,6 @@
-import { getPayload } from 'payload'
 import React from 'react'
 
-import config from '@/payload.config'
 import './styles.css'
-import { Funnel } from '@/components/Funnel'
 
 // TODO: deploy app
 // TODO: create multiple choice screen
@@ -11,18 +8,5 @@ import { Funnel } from '@/components/Funnel'
 // INFO: so we must have singe/multiple choice screens, but they have same schema
 
 export default async function HomePage() {
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-
-  const contentScreenData = await payload.findByID({
-    collection: 'content-screens',
-    id: '67af40acd898f12049d551f2',
-  })
-
-  const choiceScreenData = await payload.findByID({
-    collection: 'choice-screens',
-    id: '67af42c1d898f12049d552fd',
-  })
-
-  return <Funnel contentData={contentScreenData} choiceScreenData={choiceScreenData} />
+  return <h1>Home page</h1>
 }
